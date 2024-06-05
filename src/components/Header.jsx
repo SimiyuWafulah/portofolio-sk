a1c82fd1d6138026fc875e6b71ad5828b7efe90d
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-06-05 14:18:10 +0300

    made shanges to header ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="bg-gray-800 text-white py-4">
+    <header className="bg-slate-900 text-white py-4 fixed w-full top-0 z-10">
       <nav className="container mx-auto flex justify-between items-center">
-        <div className="text-lg font-bold">Kelvin Simiyu || SWE</div>
-        <ul className="flex space-x-4">
-          <li><Link to="/" className="hover:text-gray-400">Home</Link></li>
-          <li><Link to="/projects" className="hover:text-gray-400">Projects</Link></li>
-          <li><Link to="/contact" className="hover:text-gray-400">Contact</Link></li>
-          <li><Link to="/blog" className="hover:text-gray-400">Blog</Link></li>
+        <div className="text-lg font-bold">KS</div>
+        <ul className="flex justify-center space-x-8 md:space-x-12 lg:space-x-16">
+          <li><Link to="/" className="hover:text-teal-400">Home</Link></li>
+          <li><Link to="/" className="hover:text-teal-400">About</Link></li>
+          <li><Link to="/projects" className="hover:text-teal-400">Projects</Link></li>
+          <li><Link to="/contact" className="hover:text-teal-400">Contact</Link></li>
+          <li><Link to="/blog" className="hover:text-teal-400">Blog</Link></li>
         </ul>
       </nav>
     </header>
